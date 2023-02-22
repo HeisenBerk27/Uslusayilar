e4f52013a70a22fd41d402e74559a8c4dc80a6c6
Berk Akgül <108866525+HeisenBerk27@users.noreply.github.com>
2023-02-22 15:59:54 +0300

    Add files via upload

---
+++ src/dongu/ÜsFor.java
@@ -0,0 +1,25 @@
+package dongu;
+import java.util.Scanner;
+public class ÜsFor 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int toplam= 1;
+		System.out.println("Us alınacak sayiyi giriniz: ");
+		int ust = scanner.nextInt();
+		System.out.println("kuvvet sayisini giriniz: ");
+		int kuvvet = scanner.nextInt();
+		
+		for(int i =1; i<=kuvvet; i++)
+		{
+			toplam = toplam * ust;	
+		}
+		
+		System.out.println(toplam);
+	}
+
+}
